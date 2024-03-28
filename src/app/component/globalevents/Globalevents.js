e9a0cb82bc7e2e76f394a4159bacb5560d8d94e0
kartik_gaur <gaurdeveloper1916@gmail.com>
2024-03-28 23:12:27 +0530

    chnage

--- src/app/component/globalevents/Globalevents.js
+++ src/app/component/globalevents/Globalevents.js
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function Globalevents() {
   const [index, setIndex] = useState(0);
@@ -9,42 +11,48 @@ function Globalevents() {
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
-
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-              <img className="" src={img} alt="" />{" "}
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="" src={img} alt="" />{" "}
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="" src={img} alt="" />{" "}
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </div>
-      </div>
+<div className="container">
+  <div className="d-flex justify-content-center align-items-center">
+  <div className="col-lg-10">
+  <Slider {...settings}>
+    <div>
+      <img
+        className="w-100 img-fluid global-event-img"
+        src="https://img.freepik.com/free-photo/medium-shot-kid-cheating-school-test_23-2150104930.jpg?size=626&ext=jpg"
+        alt=""
+        style={{ maxHeight: "500px", height: "auto" }} // Adjust height as needed
+      />
     </div>
+    <div>
+      <img
+        className="w-100 img-fluid global-event-img"
+        src="https://t3.ftcdn.net/jpg/06/99/52/94/240_F_699529470_u7ZaG6otV8Cnn5WwHFc8YowT9IRXhFr8.jpg"
+        alt=""
+        style={{ maxHeight: "500px", height: "auto" }} // Adjust height as needed
+      />
+    </div>
+    <div>
+      <img
+        className="w-100 img-fluid global-event-img"
+        src="https://img.freepik.com/free-photo/horror-scene-with-eerie-classroom_23-2150975108.jpg?size=626&ext=jpg&uid=R137210381&ga=GA1.1.2086156045.1707366156&semt=sph"
+        alt=""
+        style={{ maxHeight: "500px", height: "auto" }} // Adjust height as needed
+      />
+    </div>
+  </Slider>
+</div>
+
+  </div>
+</div>
+
   );
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,17 @@ export default function RootLayout({ children }) {
       <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
       <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
-
+      <link
+  rel="stylesheet"
+  type="text/css"
+  charset="UTF-8"
+  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+/>
+<link
+  rel="stylesheet"
+  type="text/css"
+  href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+/>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar />
