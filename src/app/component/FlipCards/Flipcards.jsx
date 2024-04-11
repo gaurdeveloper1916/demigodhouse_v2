b85e0d65e3b63ff46a5c61ce72b5864789f31d9c
Ravi kumar <101349988+Ravikr2521@users.noreply.github.com>
2024-04-11 13:01:17 +0530

    changes

--- src/app/component/FlipCards/Flipcards.jsx
+++ src/app/component/FlipCards/Flipcards.jsx
@@ -14,7 +14,10 @@ const Flipcards = () => {
                 }}
                 class="card-front rounded"
               >
-                <div className=" d-flex flex-column">
+                
+
+               
+                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
                   <p
                     style={{ textAlign: "start" }}
                     className="text-white m-0 p-0"
@@ -29,19 +32,29 @@ const Flipcards = () => {
                     Tenetur explicabo deleniti repellendus{" "}
                   </p>
                 </div>
+                
               </div>
               <div class="card-back p-4 mt-2">
+                <div className="d-flex flex-column">
+
+                </div>
                 <img
                   className="w-100 rounded"
                   src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
                 ></img>
+               <div className="mt-4">
                 <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "10px" }}>
+                <p style={{ textAlign: "start", fontSize: "12px" }}>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
                   reprehenderit iste soluta distinctio voluptates inventore
                   eligendi,{" "}
                 </p>
-                <button style={{ textAlign: "end" }} className="rounded">Book Now</button>
+                <div className="d-flex justify-content-end">
+                <button style={{ textAlign: "end" }} className="rounded p-1">Book Now</button>
+
+                </div>
+                
+                </div>
               </div>
             </div>
           </div>
@@ -55,7 +68,7 @@ const Flipcards = () => {
                 }}
                 class="card-front rounded"
               >
-                <div className=" d-flex flex-column">
+                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
                   <p
                     style={{ textAlign: "start" }}
                     className="text-white m-0 p-0"
@@ -72,17 +85,26 @@ const Flipcards = () => {
                 </div>
               </div>
               <div class="card-back p-4 mt-2">
+                <div className="d-flex flex-column">
+
+                </div>
                 <img
                   className="w-100 rounded"
                   src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
                 ></img>
+               <div className="mt-4">
                 <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "10px" }}>
+                <p style={{ textAlign: "start", fontSize: "12px" }}>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
                   reprehenderit iste soluta distinctio voluptates inventore
                   eligendi,{" "}
                 </p>
-                <button style={{ textAlign: "end" }} className="rounded">Book Now</button>
+                <div className="d-flex justify-content-end">
+                <button style={{ textAlign: "end" }} className="rounded p-1">Book Now</button>
+
+                </div>
+                
+                </div>
               </div>
             </div>
           </div>
@@ -96,7 +118,7 @@ const Flipcards = () => {
                 }}
                 class="card-front rounded"
               >
-                <div className=" d-flex flex-column">
+                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
                   <p
                     style={{ textAlign: "start" }}
                     className="text-white m-0 p-0"
@@ -113,17 +135,26 @@ const Flipcards = () => {
                 </div>
               </div>
               <div class="card-back p-4 mt-2">
+                <div className="d-flex flex-column">
+
+                </div>
                 <img
                   className="w-100 rounded"
                   src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
                 ></img>
+               <div className="mt-4">
                 <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "10px" }}>
+                <p style={{ textAlign: "start", fontSize: "12px" }}>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
                   reprehenderit iste soluta distinctio voluptates inventore
                   eligendi,{" "}
                 </p>
-                <button style={{ textAlign: "end" }} className="rounded">Book Now</button>
+                <div className="d-flex justify-content-end">
+                <button style={{ textAlign: "end" }} className="rounded p-1">Book Now</button>
+
+                </div>
+                
+                </div>
               </div>
             </div>
           </div>
@@ -137,7 +168,7 @@ const Flipcards = () => {
                 }}
                 class="card-front rounded"
               >
-                <div className=" d-flex flex-column">
+                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
                   <p
                     style={{ textAlign: "start" }}
                     className="text-white m-0 p-0"
@@ -154,17 +185,26 @@ const Flipcards = () => {
                 </div>
               </div>
               <div class="card-back p-4 mt-2">
+                <div className="d-flex flex-column">
+
+                </div>
                 <img
                   className="w-100 rounded"
                   src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
                 ></img>
+               <div className="mt-4">
                 <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "10px" }}>
+                <p style={{ textAlign: "start", fontSize: "12px" }}>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
                   reprehenderit iste soluta distinctio voluptates inventore
                   eligendi,{" "}
                 </p>
-                <button style={{ textAlign: "end" }} className="rounded">Book Now</button>
+                <div className="d-flex justify-content-end">
+                <button style={{ textAlign: "end" }} className="rounded p-1">Book Now</button>
+
+                </div>
+                
+                </div>
               </div>
             </div>
           </div>
