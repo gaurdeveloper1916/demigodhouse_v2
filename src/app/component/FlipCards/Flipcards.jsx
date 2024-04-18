1b48997a9ed85eaca4dec17d3782519774325e47
kartik_gaur <gaurdeveloper1916@gmail.com>
2024-04-18 22:25:39 +0530

    chnages

--- src/app/component/FlipCards/Flipcards.jsx
+++ src/app/component/FlipCards/Flipcards.jsx
@@ -2,15 +2,58 @@ import React from "react";
 import "../FlipCards/flipcard.css";
 
 const Flipcards = () => {
-  return (
+  const sliderImageUrl = [
+    //First image url
+    {
+      url:"https://i2.wp.com/www.geeksaresexy.net/wp-content/uploads/2020/04/movie1.jpg?resize=600%2C892&ssl=1",
+      hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+      heading: 'Heading 1', paragraph: 'Description for image 1'
+    },
+    {
+      url:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2020-call-of-the-wild-1579042974.jpg?crop=0.9760858955588091xw:1xh;center,top&resize=480:*",
+        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+
+        heading: 'Heading 2', paragraph: 'Description for image 2'
+    },
+    //Second image url
+    {
+      url:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+
+        heading: 'Heading 3', paragraph: 'Description for image 3'
+    },
+    //Third image url
+    {
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU",
+        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+
+        heading: 'Heading 4', paragraph: 'Description for image 4'
+    },
+  
+    // //Fourth image url
+  
+    // {
+    //   url:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdvuww0JDC7nFRxiFL6yFiAxRJgM-1tvJTxA&usqp=CAU",
+    //     hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
+    //     heading: 'Heading 4', paragraph: 'Description for image 4'
+
+
+    // }
+  ];  return (
     <div className="container-fluid">
       <div className="d-flex gap-2"  >
-        <div className="col-lg-3 ">
-          <div class="wrapper w-100" ontouchstart="this.classList.toggle('hover');">
+       {
+        sliderImageUrl.map((item)=>{
+          return(
+            <div class="wrapper w-100" ontouchstart="this.classList.toggle('hover');">
             <div class="flipper ">
               <div
                 style={{
-                  backgroundImage: `url(https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg)`,
+                  backgroundImage: `url(${item.url})`,
                 }}
                 class="card-front rounded"
               >
@@ -31,7 +74,7 @@ const Flipcards = () => {
                 </div>
                 
               </div>
-              <div class="card-back w-100  rounded p-4 ">
+              <div class="card-back  w-100  rounded ">
                 <div className="d-flex flex-column">
 
                 </div>
@@ -39,12 +82,12 @@ const Flipcards = () => {
                   className="w-100 rounded"
                   src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
                 ></img>
-               <div className="mb-5">
+               <div className="p-3">
                 <h5 style={{ textAlign: "start" }}>Event Name</h5>
                 <p style={{ textAlign: "start", fontSize: "12px" }}>
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
                   reprehenderit iste soluta distinctio voluptates inventore
-                  eligendi,{" "}
+                  eligendi, {" "}
                 </p>
                 <div className="d-flex justify-content-end">
                 <button style={{ textAlign: "end",borderRadius:'20px',fontSize:'10px' }} className=" px-3">Book Now</button>
@@ -55,154 +98,11 @@ const Flipcards = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div class="wrapper w-100" ontouchstart="this.classList.toggle('hover');">
-            <div class="flipper ">
-              <div
-                style={{
-                  backgroundImage: `url(https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg)`,
-                }}
-                class="card-front rounded"
-              >
-                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
-                  <p
-                    style={{ textAlign: "start" }}
-                    className="text-white m-0 p-0"
-                  >
-                    Event Name
-                  </p>
-                  <p
-                    style={{ textAlign: "start", fontSize: "12px" }}
-                    className="text-white"
-                  >
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Tenetur explicabo deleniti repellendus{" "}
-                  </p>
-                </div>
-                
-              </div>
-              <div class="card-back w-100  rounded p-4 ">
-                <div className="d-flex flex-column">
-
-                </div>
-                <img
-                  className="w-100 rounded"
-                  src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
-                ></img>
-               <div className="mb-5">
-                <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "12px" }}>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-                  reprehenderit iste soluta distinctio voluptates inventore
-                  eligendi,{" "}
-                </p>
-                <div className="d-flex justify-content-end">
-                <button style={{ textAlign: "end",borderRadius:'20px',fontSize:'10px' }} className=" px-3">Book Now</button>
-
-                </div>
-                
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="wrapper w-100" ontouchstart="this.classList.toggle('hover');">
-            <div class="flipper ">
-              <div
-                style={{
-                  backgroundImage: `url(https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg)`,
-                }}
-                class="card-front rounded"
-              >
-                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
-                  <p
-                    style={{ textAlign: "start" }}
-                    className="text-white m-0 p-0"
-                  >
-                    Event Name
-                  </p>
-                  <p
-                    style={{ textAlign: "start", fontSize: "12px" }}
-                    className="text-white"
-                  >
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Tenetur explicabo deleniti repellendus{" "}
-                  </p>
-                </div>
-                
-              </div>
-              <div class="card-back w-100  rounded p-4 ">
-                <div className="d-flex flex-column">
-
-                </div>
-                <img
-                  className="w-100 rounded"
-                  src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
-                ></img>
-               <div className="mb-5">
-                <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "12px" }}>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-                  reprehenderit iste soluta distinctio voluptates inventore
-                  eligendi,{" "}
-                </p>
-                <div className="d-flex justify-content-end">
-                <button style={{ textAlign: "end",borderRadius:'20px',fontSize:'10px' }} className=" px-3">Book Now</button>
-
-                </div>
-                
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="wrapper w-100" ontouchstart="this.classList.toggle('hover');">
-            <div class="flipper ">
-              <div
-                style={{
-                  backgroundImage: `url(https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg)`,
-                }}
-                class="card-front rounded"
-              >
-                <div className=" d-flex flex-column position-absolute bottom-0 p-1">
-                  <p
-                    style={{ textAlign: "start" }}
-                    className="text-white m-0 p-0"
-                  >
-                    Event Name
-                  </p>
-                  <p
-                    style={{ textAlign: "start", fontSize: "12px" }}
-                    className="text-white"
-                  >
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Tenetur explicabo deleniti repellendus{" "}
-                  </p>
-                </div>
-                
-              </div>
-              <div class="card-back w-100  rounded p-4 ">
-                <div className="d-flex flex-column">
-
-                </div>
-                <img
-                  className="w-100 rounded"
-                  src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
-                ></img>
-               <div className="mb-5">
-                <h5 style={{ textAlign: "start" }}>Event Name</h5>
-                <p style={{ textAlign: "start", fontSize: "12px" }}>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-                  reprehenderit iste soluta distinctio voluptates inventore
-                  eligendi,{" "}
-                </p>
-                <div className="d-flex justify-content-end">
-                <button style={{ textAlign: "end",borderRadius:'20px',fontSize:'10px' }} className=" px-3">Book Now</button>
-
-                </div>
-                
-                </div>
-              </div>
-            </div>
-          </div>
+          )
+        })
+       }
+     
+          
         
       </div>
     </div>
--- src/app/multipleitemslider/page.jsx
+++ src/app/multipleitemslider/page.jsx
@@ -1,128 +1,94 @@
 // components/ImageGallery.js
-'use client'
-import React, { useState, useEffect } from 'react';
-import EventsCarousel from '../component/EventsCarousel/EventsCarousel';
-import Slider from '../component/slider/Slider';
-import Globalevents from '../component/globalevents/Globalevents';
-import Explore from '../component/explore/Explore';
-import Domesticevets from '../component/domesticevents/Domesticevets';
-import Events from '../component/events/Events';
-import Booknow from '../component/booknow/Booknow';
-import Sliderr from '../component/category/Slider';
+"use client";
+import React, { useState, useEffect } from "react";
+import EventsCarousel from "../component/EventsCarousel/EventsCarousel";
+import Slider from "../component/slider/Slider";
+import Globalevents from "../component/globalevents/Globalevents";
+import Explore from "../component/explore/Explore";
+import Domesticevets from "../component/domesticevents/Domesticevets";
+import Events from "../component/events/Events";
+import Booknow from "../component/booknow/Booknow";
+import Sliderr from "../component/category/Slider";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import SliderEvent from './sliderEvent';
-import Flipcards from '../component/FlipCards/Flipcards';
-import GetTickets from './GetTickets';
-import CarouselEvents from './slider';
+import SliderEvent from "./sliderEvent";
+import Flipcards from "../component/FlipCards/Flipcards";
+import GetTickets from "./GetTickets";
+import CarouselEvents from "./slider";
+import '../multipleitemslider/multipleslider.css'
 
 const ImageGallery = () => {
+  // const [backgroundImage, setBackgroundImage] = useState(
+  //   "https://img.freepik.com/free-photo/vibrant-scene-with-dj-techno-party_23-2150551537.jpg?ga=GA1.1.1887626236.1712805731&"
+  // );
   const [backgroundImage, setBackgroundImage] = useState('https://img.freepik.com/free-photo/vibrant-scene-with-dj-techno-party_23-2150551537.jpg?ga=GA1.1.1887626236.1712805731&');
-  const [thumbnails] = useState(['https://img.freepik.com/free-photo/vibrant-scene-with-dj-techno-party_23-2150551537.jpg?ga=GA1.1.1887626236.1712805731&', 'https://img.freepik.com/premium-photo/clubbing-dj-lively-energetic-performance-with-electronic-dance-music_734790-6060.jpg?ga=GA1.1.1887626236.1712805731&', 'https://img.freepik.com/premium-photo/dynamic-image-dj-console-energizing-party-with-vibrant-beats_99096-4039.jpg?ga=GA1.1.1887626236.1712805731&', 'https://img.freepik.com/premium-photo/dj-with-headphones-night-club-party-blue-light-people-crowd-background_968517-142771.jpg?ga=GA1.1.1887626236.1712805731&']);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [thumbnails] = useState([
+      "https://img.freepik.com/free-photo/vibrant-scene-with-dj-techno-party_23-2150551537.jpg?ga=GA1.1.1887626236.1712805731&",
+      "https://img.freepik.com/premium-photo/clubbing-dj-lively-energetic-performance-with-electronic-dance-music_734790-6060.jpg?ga=GA1.1.1887626236.1712805731&",
+      "https://img.freepik.com/premium-photo/dynamic-image-dj-console-energizing-party-with-vibrant-beats_99096-4039.jpg?ga=GA1.1.1887626236.1712805731&",
+      "https://img.freepik.com/premium-photo/dj-with-headphones-night-club-party-blue-light-people-crowd-background_968517-142771.jpg?ga=GA1.1.1887626236.1712805731&",
+  ]);
 
-  // Function to change the background image
+  useEffect(() => {
+      // Set initial background image
+      setBackgroundImage(thumbnails[currentIndex]);
+
+      // Change the background image every 5 seconds
+      const interval = setInterval(() => {
+          setCurrentIndex(current => (current + 1) % thumbnails.length);
+      }, 1000);
+      // Cleanup interval on component unmount
+      return () => clearInterval(interval);
+  }, [thumbnails, currentIndex]);
+
+  // Update backgroundImage when currentIndex changes
+  useEffect(() => {
+      setBackgroundImage(thumbnails[currentIndex]);
+  }, [currentIndex, thumbnails]);
+
   const changeBackground = (imageUrl) => {
-    setBackgroundImage(imageUrl);
+      setBackgroundImage(imageUrl);
   };
-
-  // Auto-scrolling thumbnails
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 4 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 3,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 767, min: 464 },
-      items: 2,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
-  const sliderImageUrl = [
-    //First image url
-    {
-      url:"https://i2.wp.com/www.geeksaresexy.net/wp-content/uploads/2020/04/movie1.jpg?resize=600%2C892&ssl=1",
-      hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-      heading: 'Heading 1', paragraph: 'Description for image 1'
-    },
-    {
-      url:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2020-call-of-the-wild-1579042974.jpg?crop=0.9760858955588091xw:1xh;center,top&resize=480:*",
-        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-
-        heading: 'Heading 2', paragraph: 'Description for image 2'
-    },
-    //Second image url
-    {
-      url:
-        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-
-        heading: 'Heading 3', paragraph: 'Description for image 3'
-    },
-    //Third image url
-    {
-      url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU",
-        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-
-        heading: 'Heading 4', paragraph: 'Description for image 4'
-    },
-  
-    //Fourth image url
-  
-    {
-      url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdvuww0JDC7nFRxiFL6yFiAxRJgM-1tvJTxA&usqp=CAU",
-        hoverUrl:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*",
-        heading: 'Heading 4', paragraph: 'Description for image 4'
-
-
-    }
-  ];
-  const [hoveredImageIndex, setHoveredImageIndex] = useState(null);
-
   return (
-  <div className='bg-dark'>
+    <div className="bg-dark">
       <div className="container-event-1  ">
-     
-      <div className="image-container-event">
-      <div className="event-details col-lg-6 p-5">
-        <h1 className='event-name-h1'>Event Name </h1>
-        <p>electronic typesetting, remaining it  It was popularised in the 1960s was popularised essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
-        </p>
-        <button className='book-now-event px-4'>Book Now</button>
-      </div>
-        <img src={backgroundImage} alt="Background Image" className="background-image-event " />
-        <div className="thumbnails-event p-5">
-          
-          {thumbnails.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Thumbnail ${index + 1}`}
-              className="thumbnail-event img-fluid"
-              onClick={() => changeBackground(image)}
-               
-            />
-          ))}
+        <div className="image-container-event">
+          <div className="event-details col-lg-6 p-5">
+            <h1 className="event-name-h1">Event Name </h1>
+            <p>
+              electronic typesetting, remaining it It was popularised in the
+              1960s was popularised essentially unchanged. It was popularised in
+              the 1960s with the release of Letraset sheets containing Lorem
+              Ipsum passages, and more recently with desktop publishing software
+              like Aldus
+            </p>
+            <button className="book-now-event px-4">Book Now</button>
+          </div>
+          <img
+            src={backgroundImage}
+            alt="Background Image"
+            className="background-image-event"
+          />
+          <div className="thumbnails-event p-5 backgroundImageEvent">
+            {thumbnails.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Thumbnail ${index + 1}`}
+                className="thumbnail-event img-fluid "
+                // onClick={() => changeBackground(image)}
+              />
+            ))}
+          </div>
         </div>
       </div>
-
-  
-    </div>
-   <Sliderr/>
-   <CarouselEvents/>
-   <GetTickets/>
-   <Flipcards/>
-   <EventsCarousel/>
-   {/* <div>
+      <Sliderr />
+      <CarouselEvents />
+      <GetTickets />
+      <Flipcards />
+      <EventsCarousel />
+      {/* <div>
    (
     <div className="parent p-5">
       <Carousel
@@ -157,10 +123,8 @@ const ImageGallery = () => {
     </div>
   );
    </div> */}
-   {/* <SliderEvent/> */}
-  
-  
-  </div>
+      {/* <SliderEvent/> */}
+    </div>
   );
 };
 
